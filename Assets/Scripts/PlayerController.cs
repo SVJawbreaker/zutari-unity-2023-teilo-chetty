12714dd932fc4e845ea8cfd8ee7f0a9659e84ee5
SVJawbreaker <teilochetty1003@gmail.com>
2023-03-28 15:15:20 +0200

    Player cube changes colour based on movement direction
    white is default
    green is up
    red is right
    purple is down
    cyan is left

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,22 @@ using Input = UnityEngine.Input;
 
 public class PlayerController : MonoBehaviour
 {
+    private MeshRenderer mr;
+
+    //Clockwise
+    [SerializeField] Material defMaterial;
+    [SerializeField] Material upMaterial;
+    [SerializeField] Material rightMaterial;
+    [SerializeField] Material downMaterial;
+    [SerializeField] Material leftMaterial;
+
     private Rigidbody rb;
     private float speed = 10f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        mr = GetComponent<MeshRenderer>();
     }
 
     void Update()
@@ -26,5 +36,30 @@ public class PlayerController : MonoBehaviour
 
         //Updates the player's position
         rb.MovePosition(transform.position + pInput * Time.deltaTime * speed);
+
+        materialSwitch();
+    }
+
+    private void materialSwitch()
+    {
+        //reset material
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            mr.material = defMaterial;
+
+        //Right
+        if (Input.GetAxis("Horizontal") > 0)
+            mr.material = rightMaterial;
+
+        //Left
+        if (Input.GetAxis("Horizontal") < 0)
+            mr.material = leftMaterial;
+
+        //Up
+        if (Input.GetAxis("Vertical") > 0)
+            mr.material = upMaterial;
+
+        //Down
+        if (Input.GetAxis("Vertical") < 0)
+            mr.material = downMaterial;
     }
 }
